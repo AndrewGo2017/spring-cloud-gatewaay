6cec3199783194c3a57daf1ef61cdcb19db3c81d
AndrewGo2017 <andrewmessage@mail.ru>
2019-06-16 13:23:31 +0300

    Initial commit

---
+++ src/main/java/ru/learning/springcloudgatewaay/SpringCloudGatewaayApplication.java
@@ -0,0 +1,13 @@
+package ru.learning.springcloudgatewaay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGatewaayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudGatewaayApplication.class, args);
+    }
+
+}
